6211e0f5923c175d04336c14b28938ed9ff85d1a
PawelklosPL <16582@student.pwsz.elblag.pl>
2022-02-20 21:45:17 +0100

    Simple application skeleton was created
    First commitment, let's make a good application.

---
+++ securityComponent/UsersAccount/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace securityComponent.UsersAccount
+{
+    class Account : LoginAccountData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int MyProperty { get; set; }
+    }
+
+    class LoginAccountData
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ securityComponent/UsersAccount/CreateAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace securityComponent.UsersAccount
+{
+    interface ICreateAccount
+    {
+        void CreateAccount() { }
+    }
+}
