1ef6768834c969104c009bfe8afa86aed02ae6c9
PawelklosPL <16582@student.pwsz.elblag.pl>
2022-03-02 22:36:58 +0100

    Added simple weather API.

---
+++ AdaptiveWeather/Controllers/WeatherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdaptiveWeather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpPost("get")]
+        public async Task<ActionResult<String>> GetWeather(String request)
+        {
+            return Ok("");
+        }
+    }
+}
