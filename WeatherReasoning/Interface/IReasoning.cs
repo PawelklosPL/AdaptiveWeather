545b929e7b9a6090e4db8b583db3985e59287049
PawelklosPL <16582@student.pwsz.elblag.pl>
2022-03-03 23:54:09 +0100

    Add reasoning project

---
+++ WeatherReasoning/Interface/IReasoning.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherReasoning.Interface
+{
+    internal interface IReasoning
+    {
+       void ReasoningWeatherForUser() { }
+    }
+}
